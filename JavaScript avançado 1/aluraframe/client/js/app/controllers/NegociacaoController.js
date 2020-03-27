a1b05dac9297c5b1d453791cdec4d44d1334e6ea
Gabriel Chagas <gabrielserq@outlook.com>
2020-03-27 12:04:59 -0300

    criando classes para nos ajudar e adicionando e listando as negociações

--- JavaScript avançado 1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ JavaScript avançado 1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -2,30 +2,32 @@ class NegociacaoController {
 
     constructor(){
         let $ = document.querySelector.bind(document);
-
         // recuperando dados do form
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
     }
 
     
     adicionar(event){
         event.preventDefault();
-        
         // criando negociação
-        let negociacao = new Negociacao(
-            DateHelper.textoParaData(this._inputData.value),
-            this._inputQuantidade.value,
-            this._inputValor.value
-        );
-
-        console.log(DateHelper.dataParaTexto(negociacao.data));
-
-        this.clearFields(this._inputData, this._inputQuantidade, this._inputValor);
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        // limpando campos
+        this._clearFields(this._inputData, this._inputQuantidade, this._inputValor);
     }
 
-    clearFields(data, quantidade, valor){
+    // criação de uma negociação
+    _criaNegociacao(){
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value);
+    }
+
+    // limpa campos
+    _clearFields(data, quantidade, valor){
         data.focus()
         data.value= '';
         quantidade.value = 0;
