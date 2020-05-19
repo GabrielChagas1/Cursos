bd210dd2b3d0979497625e5f5f02ceaf8562fb55
Gabriel Chagas <gabrielserq@outlook.com>
2020-05-19 16:36:55 -0300

    arrumando as rotas

--- CasaCriativa/server.js
+++ CasaCriativa/server.js
@@ -2,9 +2,16 @@ const express = require('express');
 
 const server = express();
 
+// configurar arquivos estáticos
+server.use(express.static('public'))
+
 
 server.get('/', (req, res) =>{
-    return res.sendFile(__dirname +'index.html');
+    return res.sendFile(__dirname +'/index.html');
+}); 
+
+server.get('/ideias', (req, res) =>{
+    return res.sendFile(__dirname +'/ideas.html');
 }); 
 
 
