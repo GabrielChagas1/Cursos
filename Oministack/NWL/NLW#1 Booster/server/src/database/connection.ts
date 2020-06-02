038b53895de46daacc73be5c80ec362329ac57cd
Gabriel Chagas <gabrielserq@outlook.com>
2020-06-02 13:34:49 -0300

    criando nossas migrations

--- Oministack/NWL/NLW#1 Booster/server/src/database/connection.ts
+++ Oministack/NWL/NLW#1 Booster/server/src/database/connection.ts
@@ -5,7 +5,11 @@ const connection = knex({
     client: 'sqlite3',
     connection: {
         filename: path.resolve(__dirname, 'database.sqlite'),
-    }
+    },
+    useNullAsDefault: true,
+
 });
 
 export default connection;
+
+// Migrations = histórico do banco de dados
