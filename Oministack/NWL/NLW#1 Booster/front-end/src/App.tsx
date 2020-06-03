7739a02373da7a11598d7408ff306b5e6b187f29
Gabriel Chagas <gabrielserq@outlook.com>
2020-06-03 20:00:10 -0300

    adicionando arquivos css, imagens e criando a home

--- Oministack/NWL/NLW#1 Booster/front-end/src/App.tsx
+++ Oministack/NWL/NLW#1 Booster/front-end/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 
+// importação de componentes
+import Home from './pages/Home';
+
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <Home />
   );
 }
 
 export default App;
+
+// JSX -> escrever HTML junto com JS
