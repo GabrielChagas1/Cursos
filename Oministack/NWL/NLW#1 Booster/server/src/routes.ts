90b67c6a0d3ee4adfb274452482945b74fb167ae
Gabriel Chagas <gabrielserq@outlook.com>
2020-06-02 23:35:11 -0300

    criando as controllers e as funções de listar pontos e ponto individual

--- Oministack/NWL/NLW#1 Booster/server/src/routes.ts
+++ Oministack/NWL/NLW#1 Booster/server/src/routes.ts
@@ -1,70 +1,27 @@
 import express from 'express';
 
-import knex from './database/connection';
+import PointsControler from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
+
+
 
 const routes = express.Router();
-
+const pointsController = new PointsControler();
+const itemsController = new ItemsController();
 
 // routes
 
 // rota para listar items
-routes.get('/items', async (request, response) =>{
-    const items = await knex('items').select('*');
-
-    const serializedItems = items.map(item => {
-        return {
-            id: item.id,
-            name: item.title,
-            image_url: `http:\\localhost:3333/uploads/${item.image}`,
-        };
-    })
-    
-    return response.json(serializedItems);
-});
+routes.get('/items', itemsController.index);
 
 // route para criar um ponto
-routes.post('/points', async (request, response) =>{
-    const {
-        name,
-        email,
-        whatsapp,
-        latitude,   
-        longitude, 
-        city,
-        uf, 
-        items
-    } = request.body;
+routes.post('/points', pointsController.create);
 
-    // criando uma transação
-    const trx = await knex.transaction();
+// route para trazer os pontos
+routes.get('/points', pointsController.index)
 
-    // incluindo ponto
-    const insertedIds = await trx('points').insert({
-        image: 'image-fake',
-        name,
-        email,
-        whatsapp,
-        latitude,
-        longitude, 
-        city,
-        uf
-    });
-
-    const point_id = insertedIds[0];
-    const pointItems = items.map((item_id: number) =>{
-        return {
-            item_id,
-            point_id
-        }
-    })
-
-    await trx('point_items').insert(pointItems);
-
-
-    return response.json({ success: true});
-
-});
-
+// route para trazer um ponto de coleta
+routes.get('/points/:id', pointsController.show)
 
 // exportando routes para recuperar fora desse arquivo
 export default routes;
