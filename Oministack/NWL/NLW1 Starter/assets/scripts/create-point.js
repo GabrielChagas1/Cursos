2122640d7d9cc19ba4e0cfa69f8e7e73e1e17d0c
Gabriel Chagas <gabrielserq@outlook.com>
2020-07-20 00:20:29 -0300

    criando função para popular as cidades

--- Oministack/NWL/NLW1 Starter/assets/scripts/create-point.js
+++ Oministack/NWL/NLW1 Starter/assets/scripts/create-point.js
@@ -1,6 +1,7 @@
 
 let $ = document.querySelector.bind(document);
 
+// função para popular os estados
 function populateUFs(){
     const ufSelect = document.querySelector(".create__field select[name=uf]");
 
@@ -13,18 +14,31 @@ function populateUFs(){
     }); 
 };
 
-
-function populateCitys(){
+// função para popular as cidades
+function getCities(event){
+    let citySelect = document.querySelector(".create__field select[name=city]");
+    let stateInput = document.querySelector(".create__field input[name=state]");
     
+    let indexOfSelectedState = event.target.selectedIndex;
+    stateInput.value = event.target.options[indexOfSelectedState].text;
+
+    let ufValue = event.target.value;
+    let url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
+
+    fetch(url)
+    .then((res) => res.json())
+    .then( cities => {
+        for(let city of cities){
+            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+        }
+        citySelect.disabled = false;
+    }); 
+
 }
 
 
 populateUFs();
 
-
-
-
-// $(".create__field select[name=uf]")
-//     .addEventListener("change", () => {
-//         console.log('mudei');
-//     });
+// código para chamar a função que popula as cidades
+document.querySelector(".create__field select[name=uf]")
+    .addEventListener("change", getCities);
