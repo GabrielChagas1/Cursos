aee92314da2e4c7e171a34312c7af9220cf6ad81
Gabriel Chagas <gabrielserq@outlook.com>
2020-08-06 16:40:51 -0300

    criando as funções de cada page

--- Oministack/NWL/NLW2/Starter/src/server.js
+++ Oministack/NWL/NLW2/Starter/src/server.js
@@ -1,3 +1,13 @@
+const express = require('express');
+const server = express();
+const nunjucks = require('nunjucks');
+
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
+
+});
+
 const proffys = [{
     name: "Diego Fernandes",
     avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
@@ -10,17 +20,48 @@ const proffys = [{
     time_to: [1220],
 
 
-}]
+}, 
+{
+    name: "Gabriel Chagas",
+    avatar: "https://avatars1.githubusercontent.com/u/36548770?s=460&v=4",
+    whatsapp: "89996889",
+    bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+    subject: "Skate",
+    cost: "40,00",
+    weekday: [0],
+    time_from: [720],
+    time_to: [1220],
+}];
 
-const express = require('express');
-const server = express();
-const nunjucks = require('nunjucks');
+const subjects = [
+    "Artes",
+    "Biologia",
+    "Ciências",
+    "Educação física",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química"
+    
+]
 
-nunjucks.configure('src/views', {
-    express: server,
-    noCache: true,
+const weekDays = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado"
+]
 
-})
+
+function getSubject(subjectId){
+    const arrayPosition = +subjectId -1;
+    return subjects[arrayPosition]
+}
 
 // função que vai ser executada ao ser chamada a / 
 function pageLanding(req, res) {
@@ -29,39 +70,34 @@ function pageLanding(req, res) {
 
 // função que vai ser executada ao ser chamada a page de study
 function pageStudy(req, res) {
-    const proffys = [{
-        name: "Diego Fernandes",
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-        whatsapp: "89996889",
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-        subject: "Química",
-        cost: "20",
-        weekday: [0],
-        time_from: [720],
-        time_to: [1220],
-
-
-    }, 
-    {
-        name: "Gabriel Chagas",
-        avatar: "https://avatars1.githubusercontent.com/u/36548770?s=460&v=4",
-        whatsapp: "89996889",
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-        subject: "Skate",
-        cost: "40,00",
-        weekday: [0],
-        time_from: [720],
-        time_to: [1220],
-    }];
+    const filters = req.query;
     return res.render("study.html", {
-        proffys: proffys
+        proffys,
+        filters,
+        subjects,
+        weekDays
     });
 }
 
 // função que vai ser executada ao ser chamada a page de give classes
 function pageGiveClasses(req, res) {
 
-    return res.render("give-classes.html");
+    const data = req.query;
+    const isNotEmpty = Object.keys(data).length > 0; 
+
+    // se tiver dados ele insere na lista de proffys
+    if(isNotEmpty){
+        data.subject = getSubject(data.subject);
+
+        proffys.push(data);
+        return res.redirect("/study");
+    }
+
+    // senão mostrar a página
+    return res.render("give-classes.html", {
+        subjects,
+        weekDays
+    });
 }
 
 
