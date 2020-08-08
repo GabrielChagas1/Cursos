a4996bfe7b3986ee04339d2ef520c6afe3908a45
Gabriel Chagas <gabrielserq@outlook.com>
2020-08-08 02:50:34 -0300

    exportando as funções das pages

--- Oministack/NWL/NLW2/Starter/src/server.js
+++ Oministack/NWL/NLW2/Starter/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const server = express();
 const nunjucks = require('nunjucks');
+const {pageLanding, pageStudy, pageGiveClasses} = require('./pages');
 
 nunjucks.configure('src/views', {
     express: server,
