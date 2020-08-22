a781375cde4ea9fffd92813d90a9246cbd30d2ba
Gabriel Chagas <gabrielserq@outlook.com>
2020-08-22 01:00:41 -0300

    sql do desafio - aula 3

---
+++ Youtube/DevAprender/SQL/aula-003-distinct/desafio.sql
@@ -0,0 +1,9 @@
+SELECT DISTINCT LastName
+FROM Person.Person;
+
+--Desafio 02
+ 
+-- quantos sobrenomes únicos nós temos em nossa trabalho PERSON.PERSON
+
+SELECT COUNT(DISTINCT LastName)
+FROM Person.Person;
