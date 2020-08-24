3f2830d02f5a5ef2a56bae16f14e1b5cec5b0e0a
Gabriel Chagas <gabrielserq@outlook.com>
2020-08-24 13:58:08 -0300

    desafios aula 007

--- Youtube/DevAprender/SQL/aula-007-order-by/desafio.sql
+++ Youtube/DevAprender/SQL/aula-007-order-by/desafio.sql
@@ -9,3 +9,22 @@ ORDER BY FirstName DESC;
 SELECT TOP 100 FirstName, LastName
 FROM Person.Person
 ORDER BY FirstName DESC, LastName ASC;
+
+--Desafios
+
+/*
+Desafio 1
+--Obter o productId dos 10 produtos mais caros cadastrados no sistema, listando do mais caro para o mais barato
+*/
+
+SELECT TOP 10 ProductID, ListPrice	
+FROM Production.Product 
+ORDER BY ListPrice DESC;
+
+/*
+Desafio 2
+--Obter o nome e número do produto dos produtos que tem o productID entre 1 e 4
+*/
+
+SELECT TOP 4 Name, ProductNumber
+FROM Production.Product ORDER BY ProductID ASC
