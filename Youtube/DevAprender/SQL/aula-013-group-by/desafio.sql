fafc700e49b856035cc49c632d8ca15b84e72c10
Gabriel Chagas <gabrielserq@outlook.com>
2020-08-25 20:58:21 -0300

    desafios aula sobre group by

--- Youtube/DevAprender/SQL/aula-013-group-by/desafio.sql
+++ Youtube/DevAprender/SQL/aula-013-group-by/desafio.sql
@@ -24,5 +24,35 @@ FROM Production.Product
 WHERE Color = 'Silver'
 GROUP BY Name;
 
+/*
+DESAFIOS
+*/
+
+--1) Quantas pessoas tem o mesmo sobrenome, agrupados pelo sobrenome
+SELECT MiddleName, COUNT(MiddleName) AS TOTAL
+FROM Person.Person
+GROUP BY MiddleName
+ORDER BY MiddleName ASC;
+
+--2) Em média qual é a quantidade que cada produto é vendido na loja por pedido.
+SELECT ProductID, AVG(OrderQty) AS QUANTIDADE
+FROM Sales.SalesOrderDetail
+GROUP BY ProductID
+ORDER BY QUANTIDADE DESC;
+
+--3)Qual foram as 10 vendas que no total tiveram os maiores valores de venda lineTotal por produto do maior para o menor
+SELECT  ProductID, SUM(UnitPrice) TOTAL
+FROM Sales.SalesOrderDetail
+GROUP BY ProductID
+ORDER BY TOTAL DESC;
+
+--4) Quantos produtos e qual a quantidade média dos produtos que temos cadastrados nas nossas ordem de seviços, agrupados por productID
+SELECT ProductID, 
+COUNT(ProductID) AS Total,
+AVG(OrderQty) AS Media
+FROM Production.WorkOrder
+GROUP BY ProductID;
+
+
 
 
