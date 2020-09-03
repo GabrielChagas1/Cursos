7d59b4c3655e9207cb037ca6e2edfb4f2228a3ae
Gabriel Chagas <gabrielserq@outlook.com>
2020-09-03 15:42:23 -0300

    desafio da aula 022

---
+++ Youtube/DevAprender/SQL/aula-022-operacoes-matematicas/desafio.sql
@@ -0,0 +1,19 @@
+--divindo os valores de uma linha
+SELECT UnitPrice / LineTotal
+FROM Sales.SalesOrderDetail
+
+--multiplicando os valores de uma linha
+SELECT UnitPrice * LineTotal
+FROM Sales.SalesOrderDetail
+
+--somando os valores de uma linha
+SELECT UnitPrice + LineTotal
+FROM Sales.SalesOrderDetail
+
+--subtraindo os valores de uma linha
+SELECT UnitPrice - LineTotal
+FROM Sales.SalesOrderDetail
+
+--arredondando os valores
+SELECT ROUND(LineTotal, 2)
+FROM Sales.SalesOrderDetail
