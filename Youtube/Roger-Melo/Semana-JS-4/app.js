0e6444ee40394abac6089193664e5abf14c5636c
Gabriel Chagas <gabrielserq@outlook.com>
2020-09-10 10:58:43 -0300

    exercícios map

--- Youtube/Roger-Melo/Semana-JS-4/app.js
+++ Youtube/Roger-Melo/Semana-JS-4/app.js
@@ -1,3 +1,25 @@
-const number = [1, 2, 3];
+const numbers = [1, 2, 3];
 
-console.log(number);
+// dobrando os números do array numbers
+const doubleNumbers = numbers.map( item => item * 2);
+
+// console.log(numbers, doubleNumbers);
+
+
+const products = [
+	{ name: 'Mouse Sem Fio', price: 30 },
+	{ name: 'Pen Drive', price: 25 },
+	{ name: 'Cartucho de Tinta', price: 50 },
+	{ name: 'Suporte Ergonômico para Notebook', price: 23 },
+	{ name: 'Repetidor de Sinal Wi-Fi', price: 44 }
+]
+
+const saleProducts = products.map(item => {
+    if(item.price >= 30){
+        return {name: item.name, price: item.price/2}
+    }
+
+    return item;
+});
+
+console.log(products, saleProducts);
