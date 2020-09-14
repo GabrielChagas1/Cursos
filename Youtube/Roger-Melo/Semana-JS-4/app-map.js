1c868228e37cae4e1e7756195c9e3550d1cb952c
Gabriel Chagas <gabrielserq@outlook.com>
2020-09-14 15:54:36 -0300

    arquivo com os códigos do map

---
+++ Youtube/Roger-Melo/Semana-JS-4/app-map.js
@@ -0,0 +1,26 @@
+const numbers = [1, 2, 3];
+
+// dobrando os números do array numbers
+const doubleNumbers = numbers.map( item => item * 2);
+
+// console.log(numbers, doubleNumbers);
+
+
+const products = [
+	{ name: 'Mouse Sem Fio', price: 30 },
+	{ name: 'Pen Drive', price: 25 },
+	{ name: 'Cartucho de Tinta', price: 50 },
+	{ name: 'Suporte Ergonômico para Notebook', price: 23 },
+	{ name: 'Repetidor de Sinal Wi-Fi', price: 44 }
+]
+
+// criando um array novo de produtos em que os produtos acima de 30 reais tenham o preço pela metade
+const saleProducts = products.map(item => {
+    if(item.price >= 30){
+        return {name: item.name, price: item.price/2}
+    }
+
+    return item;
+});
+
+// console.log(products, saleProducts);
