6248cd078ede9216fe3d63ab63908096044c82dd
Gabriel Chagas <gabrielserq@outlook.com>
2021-01-21 16:51:26 -0300

    api celke

--- Youtube/Celke/Semana-4/api/app.js
+++ Youtube/Celke/Semana-4/api/app.js
@@ -1,15 +1,16 @@
 // Incluindo a dependência express
 const express = require('express');
-
 const app = express();
-
 const mongoose = require('mongoose');
 
+// deixando nosso projeto interagir com dados json
 app.use(express.json());
+
+// conectando na base de dados
+mongoose.connect('mongodb://localhost:27017/myapp', {useNewUrlParser: true});
 
 
 // Routes
-
 app.post('/orcamento', async (req, res) =>{
     console.log(req.body);
     res.send('req.params.nam');
