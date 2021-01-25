e6a417888705c9973b3621860376bf41c6377f25
Gabriel Chagas <gabrielserq@outlook.com>
2021-01-25 20:20:20 -0300

    criando o componente content do wiget

--- Alura/imersao-alura/2/aluraquiz/pages/index.js
+++ Alura/imersao-alura/2/aluraquiz/pages/index.js
@@ -61,6 +61,19 @@ Widget.Header = styled.header`
   }
 `;
 
+Widget.Content = styled.div`
+  padding: 24px 32px 32px 32px;
+  & > *:first-child {
+    margin-top: 0;
+  }
+  & > *:last-child {
+    margin-bottom: 0;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+`;
 export default function Home() {
   return (
     <BackgroundImage>
