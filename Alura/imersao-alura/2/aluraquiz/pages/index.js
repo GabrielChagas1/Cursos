eee1b2f216937bb4abae6e0cdd34ddc93daf6e0c
Gabriel Chagas <gabrielserq@outlook.com>
2021-01-25 19:59:31 -0300

    criando o componente container do quiz

--- Alura/imersao-alura/2/aluraquiz/pages/index.js
+++ Alura/imersao-alura/2/aluraquiz/pages/index.js
@@ -14,12 +14,27 @@ const BackgroundImage = styled.div`
   background-position: center;
 `;
 
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+
+  @media screen and (max-width: 500px){
+    margin: auto;
+    padding: 15px;
+  }
+
+`;
+
 
 
 export default function Home() {
   return (
     <BackgroundImage>
-      teste
+      <QuizContainer>
+        teste teste teste teste
+      </QuizContainer>
     </BackgroundImage>
   );
 }
