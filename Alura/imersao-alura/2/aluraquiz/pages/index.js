8149ff2188246aeed30745b86877f5664f7a6b09
Gabriel Chagas <gabrielserq@outlook.com>
2021-01-25 19:56:41 -0300

    criando o componente de background do nosso projeto

--- Alura/imersao-alura/2/aluraquiz/pages/index.js
+++ Alura/imersao-alura/2/aluraquiz/pages/index.js
@@ -1,10 +1,25 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import db from '../db.json'
+
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.secondary};
+// `
+
+const BackgroundImage = styled.div`
+  background-image: url(${db.bg});
+  flex: 1;
+  background-size: cover;
+  background-position: center;
+`;
+
+
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <BackgroundImage>
+      teste
+    </BackgroundImage>
+  );
 }
