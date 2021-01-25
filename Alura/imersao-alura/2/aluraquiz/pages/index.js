136eb27a0a6fdae273bf6da3dee58856badbfbb2
Gabriel Chagas <gabrielserq@outlook.com>
2021-01-25 20:50:32 -0300

    importando os componentes criados dentro do index.js

--- Alura/imersao-alura/2/aluraquiz/pages/index.js
+++ Alura/imersao-alura/2/aluraquiz/pages/index.js
@@ -1,11 +1,21 @@
 import styled from 'styled-components'
+import db from '../db.json';
 
-import db from '../db.json'
+// importando o componente widget
+import {Widget} from '../src/components/Widget'
 
-// const Title = styled.h1`
-//   font-size: 50px;
-//   color: ${({ theme }) => theme.colors.secondary};
-// `
+// importando o componente footer
+import {Footer} from '../src/components/Footer'
+
+// importando o componente GitHubCorner
+import {GitHubCorner} from '../src/components/GitHubCorner'
+
+// importando o componente QuizBackground
+import {QuizBackground} from '../src/components/QuizBackground'
+
+// importando o componente QuizLogo
+import {QuizLogo} from '../src/components/QuizLogo'
+
 
 const BackgroundImage = styled.div`
   background-image: url(${db.bg});
@@ -27,53 +37,6 @@ export const QuizContainer = styled.div`
 
 `;
 
-const Widget = styled.div`
-
-  margin-top: 24px;
-  margin-bottom: 24px;
-  border: 1px solid #4caf50;
-  background-color: #1c1814;
-  border-radius: 4px;
-  overflow: hidden;
-
-  h1, h2, h3 {
-    font-size: 16px;
-    font-weight: 700;
-    line-height: 1;
-    margin-bottom: 0;
-  }
-  p {
-    font-size: 14px;
-    font-weight: 400;
-    line-height: 1;
-  }
-`;
-
-Widget.Header = styled.header`
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  padding: 18px 32px;
-  background-color: ${({ theme }) => theme.colors.primary};
-  
-  * {
-    margin: 0;
-  }
-`;
-
-Widget.Content = styled.div`
-  padding: 24px 32px 32px 32px;
-  & > *:first-child {
-    margin-top: 0;
-  }
-  & > *:last-child {
-    margin-bottom: 0;
-  }
-  ul {
-    list-style: none;
-    padding: 0;
-  }
-`;
 
 export default function Home() {
   return (
@@ -88,9 +51,14 @@ export default function Home() {
           </Widget.Content>
         </Widget>
         <Widget>
+         <Widget.Header>
           <h1>Quizes da Galera</h1>
+         </Widget.Header>
 
+         <Widget.Content>
           <p>Lorem ipsum dolor sit amet...</p>
+         </Widget.Content>
+
         </Widget>
       </QuizContainer>
     </BackgroundImage>
