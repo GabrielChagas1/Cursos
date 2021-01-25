bee0d08538b401a3ad386f6adb83b73b449deb82
Gabriel Chagas <gabrielserq@outlook.com>
2021-01-25 20:18:48 -0300

    criando o componente widget

--- Alura/imersao-alura/2/aluraquiz/pages/index.js
+++ Alura/imersao-alura/2/aluraquiz/pages/index.js
@@ -27,6 +27,27 @@ export const QuizContainer = styled.div`
 
 `;
 
+const Widget = styled.div`
+
+  margin-top: 24px;
+  margin-bottom: 24px;
+  border: 1px solid #4caf50;
+  background-color: #1c1814;
+  border-radius: 4px;
+  overflow: hidden;
+
+  h1, h2, h3 {
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 1;
+    margin-bottom: 0;
+  }
+  p {
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 1;
+  }
+`;
 
 
 export default function Home() {
