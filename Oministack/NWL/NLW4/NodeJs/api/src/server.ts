8860408f5d122ed99ad411f04f5fb791f97b16c2
Gabriel Chagas <gabrielserq@outlook.com>
2021-02-22 12:43:06 -0300

    criando o server da nossa aplicação

---
+++ Oministack/NWL/NLW4/NodeJs/api/src/server.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+
+const app = express();
+
+/* 
+GET => Busca
+POST => Salvar
+PUT => Alterar
+DELETE => Deletar
+PATCH => alteração específica
+*/
+
+/*
+    1 param => Rota(Recurso API)
+    2 param => Request, response (req, res)
+*/
+
+// criando uma rota de get
+app.get('/', (req, res) =>{
+    return res.json({message: 'Hello World - NLW#04'});
+});
+
+app.post('/', (req, res) =>{
+    return res.json({message: 'Os dados foram salvos com sucesso!'});
+});
+
+app.listen(3333, () => console.log("Server is running!"));
