5b7af86537254c250848f507cc88d385331f66bd
Gabriel Chagas <gabrielserq@outlook.com>
2021-02-24 18:42:35 -0300

    criando a rota para recuperar a lista de suveys

--- Oministack/NWL/NLW4/NodeJs/api/src/routes.ts
+++ Oministack/NWL/NLW4/NodeJs/api/src/routes.ts
@@ -1,11 +1,17 @@
 import {Router} from 'express';
+import { SurveysController } from './controllers/SurveysController';
 import { UserController } from './controllers/UserController';
 
 const router = Router();
 
 // instanciando a classe userController
 const userController = new UserController();
+const surveysController = new SurveysController();
 
 router.post("/users", userController.create)
+
+// routes para surveys
+router.post("/surveys", surveysController.create)
+router.get('/surveys', surveysController.show)
 
 export {router};
