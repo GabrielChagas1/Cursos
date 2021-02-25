98bc2e7763ec36aaa264763c1578b1fdc34b953c
Gabriel Chagas <gabrielserq@outlook.com>
2021-02-24 21:18:33 -0300

    refatorando código do server para poder utilizar teste unitários

--- Oministack/NWL/NLW4/NodeJs/api/src/server.ts
+++ Oministack/NWL/NLW4/NodeJs/api/src/server.ts
@@ -1,15 +1,4 @@
-import 'reflect-metadata';
-import express from 'express';
-import './database';
-import { router } from './routes';
-
-const app = express();
-
-// habilitando o nosso projeto a trabalhar com json
-app.use(express.json());
-
-// usando as routas que estão dentro do arquivo de routes
-app.use(router)
+import { app } from "./app";
 
 // abrindo uma conexão para o servidor.
 app.listen(3333, () => console.log("Server is running!"));
