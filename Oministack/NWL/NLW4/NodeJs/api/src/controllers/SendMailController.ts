2742a5fc27dfda1bd041bf3c6e82e122ea78c7c3
Gabriel Chagas <gabrielserq@outlook.com>
2021-02-25 22:27:51 -0300

    controller do nosso e-mail

--- Oministack/NWL/NLW4/NodeJs/api/src/controllers/SendMailController.ts
+++ Oministack/NWL/NLW4/NodeJs/api/src/controllers/SendMailController.ts
@@ -1,8 +1,10 @@
+import {resolve} from 'path';
 import { Request, Response } from 'express';
 import { getCustomRepository } from "typeorm";
 import { UsersRespository } from "../repositories/UsersRespository";
 import { SurveysRespository } from "../repositories/SurveysRespository";
 import { SurveysUsersRespository } from "../repositories/SurveysUsersRespository";
+import SendMailService from '../services/SendMailService';
 
 
 class SendMailController{
@@ -13,29 +15,59 @@ class SendMailController{
         const surveysUsersRespository = getCustomRepository(SurveysUsersRespository);
 
         // verificando se o user existe, senão ele retorna um erro
-        const userAlreadyExists = await usersRespository.findOne({ email });
-        if(!userAlreadyExists){
+        const user = await usersRespository.findOne({ email });
+        if(!user){
             return res.status(400).json({
                 error: 'User does not exists',
             });
         }
             
         // verificando se o survey existe, senão ele retorna um erro   
-        const surveysAlreadyExists = await surveysRespository.findOne({ id: survey_id });
-        if(!userAlreadyExists){
+        const survey = await surveysRespository.findOne({ id: survey_id });
+        if(!survey){
             return res.status(400).json({
                 error: 'Survey does not exists',
             });
         }
+
+        //criando um objeto com diversos valores
+        const variables = {
+            name: user.name,
+            title: survey.title,
+            description: survey.description,
+            link: process.env.URL_MAIL,
+            user_id: user.id
+        }
+
+        // recuperando o local que o meu handlebars está
+        let npsPath = resolve(__dirname, "../", "views", "emails", "npsMail.hbs");
+
+        // validando se já existe um cadastro com esse e-mail e id
+        const surveyUserAlreadyExists = await surveysUsersRespository.findOne({ 
+            where: [{user_id: user.id}, {value: null}],
+            relations: ["user", "survey"]
+        });
+
+        // se já existir um usuário cadastrado ele dispara um e-mail
+        if(surveyUserAlreadyExists){
+            await SendMailService.execute(email, survey.title, variables, npsPath);
+            return res.json(surveyUserAlreadyExists);
+        }
             
         // salvar informações no banco de dados
         const surveyUser = surveysUsersRespository.create({
-            user_id: userAlreadyExists.id,
+            user_id: user.id,
             survey_id
         });
 
         await surveysUsersRespository.save(surveyUser);
 
+         
+
+        // chamando o métod execute da nossa classe sendMailService
+        await SendMailService.execute(email, survey.title ,variables, npsPath);
+
+        // retornando o json se tudo ocorrer bem
         return res.json(surveyUser);
     }
 }
