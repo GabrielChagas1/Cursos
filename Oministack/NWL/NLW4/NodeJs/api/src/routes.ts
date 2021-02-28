bcb03645646cc53ffc8ad7adfb89b624d60d973c
Gabriel Chagas <gabrielserq@outlook.com>
2021-02-28 15:34:19 -0300

    criando rotas para o calculo de nps

--- Oministack/NWL/NLW4/NodeJs/api/src/routes.ts
+++ Oministack/NWL/NLW4/NodeJs/api/src/routes.ts
@@ -1,7 +1,9 @@
 import {Router} from 'express';
+import { AnswerController } from './controllers/AnswerController';
 import { SendMailController } from './controllers/SendMailController';
 import { SurveysController } from './controllers/SurveysController';
 import { UserController } from './controllers/UserController';
+import { NpsController } from './controllers/NpsController';
 
 const router = Router();
 
@@ -9,13 +11,23 @@ const router = Router();
 const userController = new UserController();
 const surveysController = new SurveysController();
 const sendMailController = new SendMailController();
+const answerController = new AnswerController();
+const npsController = new NpsController();
 
-router.post("/users", userController.create)
+// routes para users
+router.post("/users", userController.create);
 
 // routes para surveys
-router.post("/surveys", surveysController.create)
-router.get('/surveys', surveysController.show)
+router.post("/surveys", surveysController.create);
+router.get('/surveys', surveysController.show);
 
-router.post('/sendMail', sendMailController.execute)
+// routes para o sendMail
+router.post('/sendMail', sendMailController.execute);
+
+// routes para o answer
+router.get('/answers/:value', answerController.execute);
+
+// routes para o nps
+router.get('/nps/:survey_id', npsController.execute);
 
 export {router};
