0a8e2488fe40ac1d8d46800e0b60b2976b61fc2f
Gabriel Chagas <gabrielserq@outlook.com>
2021-03-01 15:59:05 -0300

    removendo os valores fixos e adicioando variáveis para deixar dinâmico

--- Oministack/NWL/NLW4/moveit-next/src/components/CompletedChallenges.tsx
+++ Oministack/NWL/NLW4/moveit-next/src/components/CompletedChallenges.tsx
@@ -1,10 +1,18 @@
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import { useContext } from 'react';
+
 import styles from '../styles/components/CompletedChallenges.module.css';
 
 export function CompletedChallenges(){
+
+    // recuperando o total de challenges completados usando o context
+    const { challengesCompleted } = useContext(ChallengesContext);
+
+
     return(
         <div className={styles.challengesContainer}>
             <span>Desafios Completos</span>
-            <span>0</span>
+            <span>{challengesCompleted}</span>
         </div>
     )
 }
