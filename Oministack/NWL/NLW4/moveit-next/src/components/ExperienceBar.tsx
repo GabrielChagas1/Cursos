64f7c95f5d9d1e80fdc73cc1c064a01452114493
Gabriel Chagas <gabrielserq@outlook.com>
2021-03-01 15:57:52 -0300

    removendo os valores fixos e colocando os variáveis

--- Oministack/NWL/NLW4/moveit-next/src/components/ExperienceBar.tsx
+++ Oministack/NWL/NLW4/moveit-next/src/components/ExperienceBar.tsx
@@ -1,17 +1,26 @@
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import { useContext } from 'react';
+
 import styles from '../styles/components/ExperienceBar.module.css';
 
-
 export function ExperienceBar(){
+
+    // recuperando o xp que o usuário tem e o xp total para ele passar de level
+    const { currentExperience, experienceToNextLevel} = useContext(ChallengesContext);
+
+    // calculando a porcentagem que o xp do user etá de alcançar a meta
+    const percentToNextLevel = Math.round((currentExperience * 100)) / experienceToNextLevel;
+
     return (
         <header className={styles.experienceBar}>
             <span>0 xp</span>
             <div>
-                <div style={{ width: '50%' }} />
-                <span className={styles.currentExperience} style={{left: '50%'}}>
-                    300 xp
+                <div style={{ width: `${percentToNextLevel}%` }} />
+                <span className={styles.currentExperience} style={{left: `${percentToNextLevel}%`}}>
+                    {currentExperience} xp
                 </span>
             </div>
-            <span>600 xp</span>
+            <span>{experienceToNextLevel } xp</span>
         </header>
     )
 }
