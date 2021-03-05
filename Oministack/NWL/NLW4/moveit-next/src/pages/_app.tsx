4d07fdc62eddc491dd6509450c5c255fd010ed3f
Gabriel Chagas <gabrielserq@outlook.com>
2021-03-05 02:18:31 -0300

    refatorando para recuperar os dados do cookies

--- Oministack/NWL/NLW4/moveit-next/src/pages/_app.tsx
+++ Oministack/NWL/NLW4/moveit-next/src/pages/_app.tsx
@@ -3,11 +3,8 @@ import '../styles/global.css';
 import { ChallengesProvider } from '../contexts/ChallengesContext';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    
-    <ChallengesProvider>
+  return (  
       <Component {...pageProps} />
-    </ChallengesProvider>
   ) 
 }
 
