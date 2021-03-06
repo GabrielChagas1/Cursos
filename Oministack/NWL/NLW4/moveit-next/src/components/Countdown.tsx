fbf99fa7bba7579012f166afa66216ae953eb91e
Gabriel Chagas <gabrielserq@outlook.com>
2021-03-06 02:07:08 -0300

    verificando o tema atual para trocar a cor do SVG :wrench:

--- Oministack/NWL/NLW4/moveit-next/src/components/Countdown.tsx
+++ Oministack/NWL/NLW4/moveit-next/src/components/Countdown.tsx
@@ -1,8 +1,11 @@
 import { useContext } from 'react';
 import { CountdownContext } from '../contexts/CountdownContext';
+import { ThemeContext } from '../contexts/ThemeContext';
 import styles from '../styles/components/Countdown.module.css';
 
 export function Countdown(){
+
+    const {currentTheme} = useContext(ThemeContext);
 
     const {
         minutes, 
@@ -46,19 +49,36 @@ export function Countdown(){
                 className={styles.countdownButton}
                 >
                     Ciclo Encerrado 
-                    <img src="/icons/check.svg" alt="check svg"/>
+                    <img src="/icons/check.svg" alt="check svg" />
 
                 </button>
             ) : (
                 <>
                     { !isActive ? (
+
+                        currentTheme === 'theme-dark' ?
+                    
+                        <button 
+                        type="button" 
+                        className={`${styles.countdownButton} ${styles.countdownButtonDark}`}
+                        onClick={startCountdown}
+                        >
+                            Iniciar um ciclo
+                            <img src="/icons/clock.svg" alt="clock svg"/>
+                        </button>
+
+                        :
+
                         <button 
                         type="button" 
                         className={styles.countdownButton}
                         onClick={startCountdown}
                         >
                             Iniciar um ciclo
+                            <img src="/icons/clock.svg" alt="clock svg"/>
                         </button>
+
+
                         ) : (
                             <button 
                             type="button" 
@@ -66,6 +86,7 @@ export function Countdown(){
                             onClick={resetCountdown}
                             >
                                 Abandonar um ciclo
+                                <img src="/icons/clock.svg" alt="clock svg"/>
                             </button>
                         )
                     }
