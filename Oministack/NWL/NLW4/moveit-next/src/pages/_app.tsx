ba2c54b726186605766eb9f468811414f65bda8d
Gabriel Chagas <gabrielserq@outlook.com>
2021-03-06 01:30:49 -0300

    adicionando o provider do theme por volta da aplicação :fire:

--- Oministack/NWL/NLW4/moveit-next/src/pages/_app.tsx
+++ Oministack/NWL/NLW4/moveit-next/src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import '../styles/global.css';
 
-import { ChallengesProvider } from '../contexts/ChallengesContext';
+import { ThemeProvider } from '../contexts/ThemeContext';
 
 function MyApp({ Component, pageProps }) {
   return (  
+    <ThemeProvider>
       <Component {...pageProps} />
+    </ThemeProvider>
   ) 
 }
 
