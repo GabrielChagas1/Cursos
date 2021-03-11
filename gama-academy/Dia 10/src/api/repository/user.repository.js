4444a618a78adc7965200f128d50557c720e2297
Gabriel Chagas <gabrielserq@outlook.com>
2021-03-11 20:16:13 -0300

    arquivos para a aula do dia 10

---
+++ gama-academy/Dia 10/src/api/repository/user.repository.js
@@ -0,0 +1,23 @@
+const database = require('../../helpers/database')
+const crypto = require('../../helpers/mycrypto')
+
+const save = async (user) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+
+            const encrypt = await crypto.encryptPassword(user.password, null)
+            user.salt = encrypt.salt
+            user.password = encrypt.encryptedPassword
+           
+            const sqlStatement = `INSERT INTO users (username, password, salt) VALUES ("${user.username}", "${user.password}", "${user.salt}");`            
+            const result = await database.execute(sqlStatement)
+
+            resolve(result)
+        } catch (error) {
+            console.error(error)
+            reject(error)      
+        }
+    })
+}
+
+module.exports = { save }
