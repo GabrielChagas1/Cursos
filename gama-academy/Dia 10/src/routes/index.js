3942ed54c4a6ac4244cedcbeabff7c6adc6d7c71
Gabriel Chagas <gabrielserq@outlook.com>
2021-03-12 18:44:30 -0300

    add o swagger nas configurações do controller

--- gama-academy/Dia 10/src/routes/index.js
+++ gama-academy/Dia 10/src/routes/index.js
@@ -1,18 +1,39 @@
-
+const Joi = require('joi')
 const { status } = require('../api/controllers/app.controller')
 const authcontroller = require('../api/controllers/auth.controller')
 const usercontroller = require('../api/controllers/user.controller')
 
+const {loginRequestDTO, loginResponseDTO } = require('../api/models/dto/auth.dto')
+
 const root = {
     method: 'GET',
     path: '/',
-    handler: status
+    handler: status,
+    options: {
+        tags: ['api'],
+        description: 'Verificação do status da aplicação',
+        notes: 'Pode ser utilizado sempre que outra aplicação estiver monitorando'
+    }
 }
 
 const login = {
     method: 'POST',
     path: '/login',
-    handler: authcontroller.login
+    handler: authcontroller.login,
+    options: {
+        tags: ['api', 'login'],
+        description: 'Rota de autenticação',
+        notes: 'Anotações da rota',
+        validate:{
+            payload: loginRequestDTO
+        },
+        response: {
+            status:{
+                200: loginResponseDTO,
+                400: Joi.any()
+            }
+        }
+    }
 }
 
 const validate = {
