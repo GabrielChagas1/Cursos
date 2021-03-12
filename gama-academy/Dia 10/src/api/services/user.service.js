4b8d393e6e7caa02072e54ccac599dfad6b44e6e
Gabriel Chagas <gabrielserq@outlook.com>
2021-03-12 18:43:15 -0300

    validando se não existe um usuário já cadastrado

--- gama-academy/Dia 10/src/api/services/user.service.js
+++ gama-academy/Dia 10/src/api/services/user.service.js
@@ -6,10 +6,15 @@ const createAccount = async (newUser) => {
 
 
     //TODO: Verficar se usuario já existe
-    const user = await repository.findOne(newUser.username);
+
+    //TODO: tmb senti falta de uns detalhezinhos como try  catch ou retornos de erros … mas ideia central ta top
+
+     //TODO:uma ultima coisa, mas ai seria mais profissa mesmo de sacar… o seu resolve poderia retornar um new User pra garantir a especificação do modelo
+
+    const user = await repository.find(newUser.username);
 
     if(!user)
-        const result = await repository.save(newUser)
+        result = await repository.save(newUser)
         return result;
 
 }
