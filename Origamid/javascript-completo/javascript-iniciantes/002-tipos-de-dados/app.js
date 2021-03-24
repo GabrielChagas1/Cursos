cbb38084c83635efbcf16bd5dc692aed3c365d49
Gabriel Chagas <gabrielserq@outlook.com>
2021-03-24 01:09:30 -0300

    aula sobre tipos de dados

---
+++ Origamid/javascript-completo/javascript-iniciantes/002-tipos-de-dados/app.js
@@ -0,0 +1,26 @@
+
+let nome = 'Gabriel'; //string
+let idade = 30; //number
+let isDeveloper = true; //boolean
+let time; //undefined
+let comida = null //null
+let simbolo = Symbol() //Symbol
+let novoObjeto = {} //objeto
+ 
+console.log(`${nome} ${idade}`);
+
+// Declare uma variável contendo uma string
+let favoritePlayer = 'Nikola Jokic';
+// Declare uma variável contendo um número dentro de uma string
+let age = '25';
+// Declare uma variável com a sua idade
+let ageMe = 21;
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+let nameMe = 'Gabriel',
+    lastName = 'Chagas'
+let fullName = `${nameMe} ${lastName}`;
+// Coloque a seguinte frase em uma variável: It's time
+let phrase = `It's time`; 
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof comida);
