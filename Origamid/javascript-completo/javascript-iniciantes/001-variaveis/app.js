d4de4ff2e613b3b7c9447876101f1a8f1d320778
Gabriel Chagas <gabrielserq@outlook.com>
2021-03-23 23:19:26 -0300

    aula sobre variáveis

---
+++ Origamid/javascript-completo/javascript-iniciantes/001-variaveis/app.js
@@ -0,0 +1,36 @@
+console.log("oi");
+
+var nome = 'André';
+let idade = 21;
+const isDeveloper = true;
+console.log(nome, idade, isDeveloper);
+
+let preco = 25;
+let quantidade = 5;
+let totalPreco = preco * quantidade;
+console.log(totalPreco);
+
+// utilizar vírgula para criar várias váriaveis sem precisar repetir a palavra var, let ou const
+var name = 'Gabriel',
+years = 21,
+isMen = true;
+
+// hoisting => apesar da variável ser criado no final do códido, ela fica no "escopo", mas o valor atribuido não.
+
+// EXERCICÍOS
+
+// Declarar uma variável com o seu nome
+let name = 'Gabriel Chagas';
+// Declarar uma variável com a sua idade
+let years = '21';
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+let foodFavorite;
+// Atribuir valor a sua comida favorita
+foodFavorite = 'Hamburguer'
+// Declarar 5 variáveis diferentes sem valores
+let isDeveloper,
+    salary,
+    favoritePlayer,
+    favoriteSport,
+    favoriteSong;
