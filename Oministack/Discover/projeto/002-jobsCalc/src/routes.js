761adf4d222e5b27e0ea7f9184a51e95e6c6643f
Gabriel Chagas <gabrielserq@outlook.com>
2021-03-28 00:09:55 -0300

    refatorando as rotas

--- Oministack/Discover/projeto/002-jobsCalc/src/routes.js
+++ Oministack/Discover/projeto/002-jobsCalc/src/routes.js
@@ -1,19 +1,30 @@
 const express = require('express');
 const routes = express.Router();
 
-const basePath = `${__dirname}/views`;
+// pegando o caminho absoluto das páginas
+const views = `${__dirname}/views/`;
+
+const profile = {
+  name: "Gabriel",
+  avatar: "https://avatars.githubusercontent.com/u/36548770?v=4",
+  'monthly-budget': 300,
+  'hours-per-day': 5,
+  'days-per-week': 5,
+  'vacation-per-year': 4,
+
+}
 
 // route para a página de index
-routes.get('/', (req, res) => res.sendFile(`${basePath}/index.html`));
+routes.get('/', (req, res) => res.render(`${views}index`));
 
 // route para a página job
-routes.get('/job', (req, res) => res.sendFile(`${basePath}/job.html`));
+routes.get('/job', (req, res) => res.render(`${views}job`));
 
 // route para a página de job-edit
-routes.get('/job/edit', (req, res) => res.sendFile(`${basePath}/job-edit.html`));
+routes.get('/job/edit', (req, res) => res.render(`${views}job-edit`));
 
 // route para a página de profile
-routes.get('/profile', (req, res) => res.sendFile(`${basePath}/profile.html`));
+routes.get('/profile', (req, res) => res.render(`${views}profile`, { profile }));
 
 
 // devolvendo para fora alguma propriedades
