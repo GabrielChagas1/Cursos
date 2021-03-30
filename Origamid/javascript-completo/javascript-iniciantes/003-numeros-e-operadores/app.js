44d41e48d7341a69f7ec8297a31e17c6084ab724
Gabriel Chagas <gabrielserq@outlook.com>
2021-03-29 21:06:46 -0300

    anotações aula 003

--- Origamid/javascript-completo/javascript-iniciantes/003-numeros-e-operadores/app.js
+++ Origamid/javascript-completo/javascript-iniciantes/003-numeros-e-operadores/app.js
@@ -1,2 +1,50 @@
 var idade = 23;
+var gols = 28;
+var pi = 3.14;
 
+//* exponenciação
+var exp = 2e10;
+
+//* operadores aritméticos
+var soma = 5 + 5;
+let subtracao = 5 - 6;
+let divisao = 100 / 2;
+let multiplicacao = 100 * 2;
+let expoente = 2 ** 4;
+let modulo =  15 % 4;
+
+//* sinal de + em strings serve para concatenar
+
+//* Operadores aritméticos unários
+let incremento = 5;
+console.log(incremento++)
+console.log(incremento)
+
+var incremento2 = 5;
+console.log(++incremento)
+console.log(incremento)
+
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20;
+console.log(total);
+//* 35
+
+// Crie duas expressões que retornem NaN
+let teste1 = '78s';
+let teste2 = '78';
+
+console.log(isNaN(teste1))
+console.log(isNaN(teste2))
+
+// Somar a string '200' com o número 50 e retornar 250
+console.log(+'200' + 50);
+
+// Incremente o número 5 e retorne o seu valor incrementado
+let incrementar = 5;
+console.log(++incrementar);
+
+// Como dividir o peso por 2?
+var numero = +'80' / 2;
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+console.log(peso)
