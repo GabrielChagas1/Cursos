cd91f7c3a1c44c94b7fe735802bd98f6ebdc41f1
Gabriel Chagas <gabrielserq@outlook.com>
2021-03-30 21:33:37 -0300

    anotações aula de boolean e condicionais

--- Origamid/javascript-completo/javascript-iniciantes/004-boolean-e-condicional/app.js
+++ Origamid/javascript-completo/javascript-iniciantes/004-boolean-e-condicional/app.js
@@ -1,6 +1,47 @@
-let isDeveloper = true;
+// Verifique se a sua idade é maior do que a de algum parente
+// Dependendo do resultado coloque no console 'É maior', 'É igual' ou 'É menor'
+let y = 21, x = 30;
 
-if(isDeveloper)
-  console.log(true);
-else
-  console.log(false)
+if(y > x){
+  console.log('É maior');
+}else if(y === x){
+  console.log('É igual');
+}else{
+  console.log('É menor');
+}
+
+// Qual valor é retornado na seguinte expressão?
+var expressao = (5 - 2) && (5 - ' ') && (5 - 2);
+
+// Verifique se as seguintes variáveis são Truthy ou Falsy
+var nome = 'Andre';
+var idade = 28;
+var possuiDoutorado = false;
+var empregoFuturo;
+var dinheiroNaConta = 0;
+
+console.log(`Nome = ${!!nome}`)
+console.log(`Idade = ${!!idade}`)
+console.log(`possuiDoutorado = ${!!possuiDoutorado}`)
+// console.log(`empregoFuturo = ${!!emprego}`)
+console.log(`dinheiroNaConta = ${!!dinheiroNaConta}`)
+
+// Compare o total de habitantes do Brasil com China (valor em milhões)
+var brasil = 207;
+var china = 1340;
+
+brasil >= china ? console.log('É sim!') : console.log('Não é')
+
+// O que irá aparecer no console?
+if(('Gato' === 'gato') && (5 > 2)) {
+  console.log('Verdadeiro');
+} else {
+  console.log('Falso');
+}
+
+// O que irá aparecer no console?
+if(('Gato' === 'gato') || (5 > 2)) {
+  console.log('Gato' && 'Cão');
+} else {
+  console.log('Falso');
+}
