2377a55367ea2c7c826f176cbcf84530ad88efe4
Gabriel Chagas <gabrielserq@outlook.com>
2021-04-13 22:21:12 -0300

    retirando o método index do job e passando para o controller do dashboard

--- Oministack/Discover/projeto/002-jobsCalc/src/controllers/JobController.js
+++ Oministack/Discover/projeto/002-jobsCalc/src/controllers/JobController.js
@@ -5,32 +5,6 @@ const jobUtils = require('../utils/JobUtils');
 const Profile = require('../model/Profile');
 
 module.exports = {
-  index(req, res) {
-    // recuperando os jobs
-    const jobs = Job.get();
-    
-    // recuperado os dados do profile
-    const profile = Profile.get();
-
-    // utilizando o map para adicionar os dias que faltam e o status do job
-    const updatedJobs = jobs.map((job) => {
-
-      // recuperando os dias que faltam para a entrega do projeto
-      const remaining = jobUtils.remainingDays(job);
-
-      // recuperando o status do projeto.
-      const status = remaining <= 0 ? 'done' : 'progress';
-
-      return {
-        ...job,
-        remaining,
-        status,
-        budget: jobUtils.calculateBudget(job, profile["value-hour"])
-      };
-    });
-    res.render(`index`, { jobs: updatedJobs });
-  },
-
   save(req, res){
 
     // recuperando todos os jobs
