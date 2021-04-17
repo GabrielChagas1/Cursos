0b4d2720726918c966f2f6163336b1e1ebcd84f5
Gabriel Chagas <gabrielserq@outlook.com>
2021-04-17 20:55:07 -0300

    refatorando o profile para recuperar os dados do banco de dados

--- Oministack/Discover/projeto/002-jobsCalc/src/model/Profile.js
+++ Oministack/Discover/projeto/002-jobsCalc/src/model/Profile.js
@@ -1,16 +1,25 @@
-let data = {
-  name: "Gabriel",
-  avatar: "https://github.com/gabrielchagas1.png",
-  'monthly-budget': 1500,
-  'hours-per-day': 5,
-  'days-per-week': 5,
-  'vacation-per-year': 4,
-  'value-hour': 70,
-};
+const database = require('../db/config');
 
 module.exports = {
-  get() {
-    return data;
+  async get() {
+    // iniciando o banco de dados
+    const db = await database();
+
+    // buscando informações do profile
+    const profile = await db.get(`SELECT * FROM profile`);
+
+    // fechando a conexão
+    db.close();
+
+    return {
+      name: profile.name,
+      avatar: profile.avatar,
+      'monthly-budget': profile.monthly_budget,
+      'day-per-week': profile.days_per_week,
+      'hours-per-day': profile.hours_per_day,
+      'vacation-per-year': profile.vacation_per_year,
+      'value-hour': profile.value_hour
+    };
   },
 
   update(newData){
