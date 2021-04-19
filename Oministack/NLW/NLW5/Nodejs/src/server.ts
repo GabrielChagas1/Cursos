e7091a2b2b7a6a2eb85d7d232169600c0b76cc61
Gabriel Chagas <gabrielserq@outlook.com>
2021-04-19 20:51:29 -0300

    criando uma rota post para teste

--- Oministack/NLW/NLW5/Nodejs/src/server.ts
+++ Oministack/NLW/NLW5/Nodejs/src/server.ts
@@ -6,5 +6,11 @@ app.listen(3333, () => console.log('Server running on http://localhost:3333'));
 
 app.get('/', (req, res) =>{
   // res.send('Hello World');
-  res.json({message: "Olá NLW 05!"})
+  return res.json({message: "Olá NLW 05!"})
+});
+
+app.post('/', (req, res) =>{
+  return res.json({
+    message: 'Usuário cadastrado com sucesso!'
+  });
 })
