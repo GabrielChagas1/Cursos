880abbb6f2d4afed64a85a4ddbab4b2550625650
Gabriel Chagas <gabrielserq@outlook.com>
2021-04-21 14:14:48 -0300

    chore: criando rota de post para cadastrar um user

--- Oministack/NLW/NLW5/Nodejs/src/routes.ts
+++ Oministack/NLW/NLW5/Nodejs/src/routes.ts
@@ -1,13 +1,20 @@
   
 import { Router } from 'express';
 import { SettingsController } from './controllers/SettingsController';
+import { UsersController } from './controllers/UsersController';
 
 const routes = Router();
 
 //criando um objeto controller de settings 
-const settingsController = new SettingsController
+const settingsController = new SettingsController();
+
+// criando um objeto controller para users
+const usersController = new UsersController();
 
 // rota para cadastrar um settings
 routes.post("/settings",settingsController.create);
+
+// rota para cadastrar um usuário
+routes.post("/user", usersController.create)
 
 export { routes };
