ea2d4c568f670af45d18dc34ecd93d9b00e302de
Gabriel Chagas <gabrielserq@outlook.com>
2021-04-22 17:20:18 -0300

    feat: criando a tarefa que conecta a aplicação com o socket.io

--- Oministack/NLW/NLW5/Nodejs/src/server.ts
+++ Oministack/NLW/NLW5/Nodejs/src/server.ts
@@ -1,14 +1,20 @@
 import express from 'express';
-
 import {routes} from './routes';
+import {createServer} from "http";
+import{Server, Socket} from "socket.io"
 
 // importando o database
 import "./database";
 
 const app = express();
+// criando protocolo http
+const http = createServer(app);
+
+// criando protocolo de web socket
+const io = new Server(http);
 
 // habilitando o nosso projeto a trabalhar com json
-app.use(express.json())
+app.use(express.json());
 
 // linkando as rotas criadas
 app.use(routes);
