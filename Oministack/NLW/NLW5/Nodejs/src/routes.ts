db5c5127689e358e5329e84895765bae7d03b2df
Gabriel Chagas <gabrielserq@outlook.com>
2021-04-22 09:29:10 -0300

    feat: adicionando rotas para messages

--- Oministack/NLW/NLW5/Nodejs/src/routes.ts
+++ Oministack/NLW/NLW5/Nodejs/src/routes.ts
@@ -1,5 +1,6 @@
   
 import { Router } from 'express';
+import { MessagesController } from './controllers/MessagesController';
 import { SettingsController } from './controllers/SettingsController';
 import { UsersController } from './controllers/UsersController';
 
@@ -11,10 +12,17 @@ const settingsController = new SettingsController();
 // criando um objeto controller para users
 const usersController = new UsersController();
 
+// criando um objeto controller para messages
+const messagesController = new MessagesController();
+
 // rota para cadastrar um settings
 routes.post("/settings",settingsController.create);
 
 // rota para cadastrar um usuário
-routes.post("/users", usersController.create)
+routes.post("/users", usersController.create);
+
+// rota pra uma message
+routes.post("/messages", messagesController.create);
+routes.get("/messages/:id", messagesController.showByUser);
 
 export { routes };
