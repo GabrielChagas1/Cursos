5bb7eea997851b15ce6010b719d79f14ce0ef099
Gabriel Chagas <gabrielserq@outlook.com>
2021-04-22 17:29:28 -0300

    feat: definindo o caminho dos arquivos públicos

--- Oministack/NLW/NLW5/Nodejs/src/server.ts
+++ Oministack/NLW/NLW5/Nodejs/src/server.ts
@@ -1,12 +1,23 @@
 import express from 'express';
 import {routes} from './routes';
 import {createServer} from "http";
-import{Server, Socket} from "socket.io"
+import{Server, Socket} from "socket.io";
+import path from "path";
 
 // importando o database
 import "./database";
 
 const app = express();
+
+// definindo os arquivos publicos da aplicação
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+app.set("views", path.join(__dirname, "..", "public"));
+
+// setando html para a view engine
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+
 // criando protocolo http
 const http = createServer(app);
 
