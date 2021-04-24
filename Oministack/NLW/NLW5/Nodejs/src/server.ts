f63f8d6f865e124d31aedea1b60cd92ede0b67ba
Gabriel Chagas <gabrielserq@outlook.com>
2021-04-24 18:54:06 -0300

    refactor: refatorando arquivo de server

--- Oministack/NLW/NLW5/Nodejs/src/server.ts
+++ Oministack/NLW/NLW5/Nodejs/src/server.ts
@@ -1,5 +1,6 @@
 import { http } from "./http";
-import "./websocket/client";
+import "./websockets/client";
+import "./websockets/admin";
 
 // iniciando o servidor
 http.listen(3333, () => console.log('Server running on http://localhost:3333'));
