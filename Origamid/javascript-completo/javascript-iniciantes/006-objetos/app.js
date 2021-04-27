6f14ee3e6d5e1cf67d2acf13fcb4c4b5c360b48b
Gabriel Chagas <gabrielserq@outlook.com>
2021-04-27 20:51:33 -0300

    chore: anotações aula 07 tudo é objeto

--- Origamid/javascript-completo/javascript-iniciantes/006-objetos/app.js
+++ Origamid/javascript-completo/javascript-iniciantes/006-objetos/app.js
@@ -1,30 +1,56 @@
 'use strict'
 
-let pessoa = {
-  nome: 'Gabriel Chagas',
-  idade: 21,
-  profissao: 'Desenvolvedor',
-  possuiFaculdade: true
-};
+// let pessoa = {
+//   nome: 'Gabriel Chagas',
+//   idade: 21,
+//   profissao: 'Desenvolvedor',
+//   possuiFaculdade: true
+// };
 
-let quadrado = {
-  lados:4,  
-  area(lado){
-    return lado * lado
-  },
+// let quadrado = {
+//   lados:4,  
+//   area(lado){
+//     return lado * lado
+//   },
 
-  perimetro(lado){
-    return this.lados * lado;
-  },
+//   perimetro(lado){
+//     return this.lados * lado;
+//   },
 
-  getPi(){
-    return Math.PI.toFixed(2);
-  },
+//   getPi(){
+//     return Math.PI.toFixed(2);
+//   },
 
-  getRandomNumber(){
-    let number = Math.random() * 10;
-    let rounded = Math.floor(number);
+//   getRandomNumber(){
+//     let number = Math.random() * 10;
+//     let rounded = Math.floor(number);
 
-    return `${number} ${rounded}`;
-  }
-} 
+//     return `${number} ${rounded}`;
+//   }
+// } 
+
+// tudo é objeto
+
+let nome = "Gabriel";
+
+let nomeMinusculo = nome.toLowerCase();
+
+let num = 11.8;
+
+var btn = document.querySelector(".btn");
+btn.addEventListener('click', () => {
+ btn.classList.add('ativo')
+ console.log(btn.innerHTML)
+});
+
+// nomeie 3 propriedades ou métodos de strings
+let nome = "Gabriel"
+// fixed
+// length
+// slice
+ 
+// nomeie 5 propriedades ou métodos de elementos do DOM
+var btn = document.querySelector('.btn');
+
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
