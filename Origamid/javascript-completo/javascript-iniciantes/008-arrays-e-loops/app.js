8866baa191d43fa17b4d5e0d4fe0a6c297ebfa96
Gabriel Chagas <gabrielserq@outlook.com>
2021-04-29 09:49:01 -0300

    chore: fazendo atividades sobre arrays e loops

--- Origamid/javascript-completo/javascript-iniciantes/008-arrays-e-loops/app.js
+++ Origamid/javascript-completo/javascript-iniciantes/008-arrays-e-loops/app.js
@@ -1,27 +1,58 @@
 'use strict'
 
-var videoGames = ["PS4", "Xbox One", "Switch"];
+// var videoGames = ["PS4", "Xbox One", "Switch"];
 
-console.log(videoGames.pop()); 
+// console.log(videoGames.pop()); 
 
-videoGames.push("3DS");
+// videoGames.push("3DS");
 
-console.log(videoGames.length)
-console.log(`último elemento: ${videoGames[videoGames.length - 1]}`)
-
-
-console.log(videoGames);
+// console.log(videoGames.length)
+// console.log(`último elemento: ${videoGames[videoGames.length - 1]}`)
 
 
-let total = 10;
-let tabuada = 5;
+// console.log(videoGames);
 
-for (let i = 1; i <= total; i++) {
-  console.log(`${tabuada} x ${i} = ${tabuada*i}`)
-}
 
-var i = 0;
-while(i <= 100){
-  console.log(i);
-  i = i + 5;
-}
+// let total = 10;
+// let tabuada = 5;
+
+// for (let i = 1; i <= total; i++) {
+//   console.log(`${tabuada} x ${i} = ${tabuada*i}`)
+// }
+
+// var i = 0;
+// while(i <= 100){
+//   console.log(i);
+//   i = i + 5;
+// }
+
+
+// let frutas = ['Banana', 'Pera', 'Maçã', 'Abacaxi', 'Uva']
+
+// frutas.forEach((item, index, array) =>{
+//   frutas.pop()
+//   console.log(item, index, array)
+// });
+
+
+// Crie uma array com os anos que o Brasil ganhou a copa
+// 1959, 1962, 1970, 1994, 2002
+
+let copas = ['1959', '1962', '1970', '1994', '2002'];
+
+// Interaja com a array utilizando um loop, para mostrar
+// no console a seguinte mensagem, `O brasil ganhou a copa de ${ano}`
+
+copas.forEach((copa, index) =>{
+  console.log(`O Brasil ganhou a ${++index}° em ${copa}`);
+});
+
+// Interaja com um loop nas frutas abaixo e pare ao chegar em Pera
+var frutas = ['Banana', 'Maçã', 'Pera', 'Uva', 'Melância']
+
+// Coloque a última fruta da array acima em uma variável,
+// sem remover a mesma da array.
+
+let lasfFruit = frutas[frutas.length - 1];
+
+console.log(lasfFruit);
