e75e9180bd16b7e90d563c71dbc2826eae74a16a
Gabriel Chagas <gabrielserq@outlook.com>
2021-05-17 11:47:38 -0300

    chore: anotações sobre a primeira aula de seleção de elementos em javascript

---
+++ Origamid/javascript-completo/dom-iniciantes/003-selecionar-o-dom/assets/js/script.js
@@ -0,0 +1,19 @@
+let animais = document.getElementById('animais');
+let contato = document.getElementById('contato');
+// console.log(animais);
+
+const gridSection = document.getElementsByClassName('grid-section');
+// console.log(gridSection);
+
+
+const primeiraLi = document.querySelector('li');
+console.log(primeiraLi);
+
+let primeiraUl = document.querySelector('ul');
+console.log(primeiraUl);
+
+let linksInternos = document.querySelectorAll('[href^="#"]');
+console.log(linksInternos);
+
+let imagesProject = document.querySelectorAll('img');
+console.log(imagesProject[1])
