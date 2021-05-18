fdf215281948700176e8fe4301e0e1ab95ebe1c7
Gabriel Chagas <gabrielserq@outlook.com>
2021-05-18 12:59:04 -0300

    chore: anotações e exercícios sobre ForEach e arrow function

--- Origamid/javascript-completo/dom-iniciantes/004-foreach-e-arrow/assets/js/script-aula.js
+++ Origamid/javascript-completo/dom-iniciantes/004-foreach-e-arrow/assets/js/script-aula.js
@@ -1,33 +1,8 @@
-// let animais = document.getElementById('animais');
-// let contato = document.getElementById('contato');
-// console.log(animais);
+const imgs = document.querySelectorAll('img');
+// console.log(imgs)
+// imgs.forEach((item, index) => console.log(item.src, index))
 
-// let gridSection = document.getElementsByClassName('grid-section');
-// console.log(gridSection);
+const titulo = document.getElementsByClassName('titulo');
+const tituloArray = Array.from(titulo);
 
-
-// const primeiraLi = document.querySelector('li');
-// console.log(primeiraLi);
-
-// let primeiraUl = document.querySelector('ul');
-// console.log(primeiraUl);
-
-// let linksInternos = document.querySelectorAll('[href^="#"]');
-// console.log(linksInternos);
-
-// let imagesProject = document.querySelectorAll('img');
-// console.log(imagesProject[1]);
-
-const gridSectionHTML = document.getElementsByClassName('grid-section');
-
-const gridSectionNode = document.querySelectorAll('.grid-section');
-
-console.log(gridSectionHTML);
-console.log(gridSectionNode);
-
-gridSectionNode.forEach((item) => console.log(item.querySelector('.titulo').innerHTML));
-
-// convertando um array like em um array de verdade
-const arrayGrid = Array.from(gridSectionHTML);
-
-arrayGrid.forEach((item) => console.log(item.querySelector('.titulo').innerHTML))
+tituloArray.map((item) => console.log(item.innerHTML));
