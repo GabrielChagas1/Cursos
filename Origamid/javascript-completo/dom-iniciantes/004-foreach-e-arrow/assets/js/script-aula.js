611ccfd9f541250503b018fcbff2bf247fcfa771
Gabriel Chagas <gabrielserq@outlook.com>
2021-05-18 13:51:22 -0300

    chore: anotações aula sobre classes e atributos

--- Origamid/javascript-completo/dom-iniciantes/004-foreach-e-arrow/assets/js/script-aula.js
+++ Origamid/javascript-completo/dom-iniciantes/004-foreach-e-arrow/assets/js/script-aula.js
@@ -1,8 +1,15 @@
-const imgs = document.querySelectorAll('img');
-// console.log(imgs)
-// imgs.forEach((item, index) => console.log(item.src, index))
+const menu = document.querySelector('.menu');
 
-const titulo = document.getElementsByClassName('titulo');
-const tituloArray = Array.from(titulo);
+menu.classList.add('ativo');
+menu.classList.remove('azul');
+menu.classList.toggle('azul');
 
-tituloArray.map((item) => console.log(item.innerHTML));
+const animais = document.querySelector('.animais ');
+console.log(animais.attributes)
+
+
+const img = document.querySelector('img');
+img.setAttribute('alt', 'Raposa')
+console.log(img.getAttribute('alt'));
+const isAlt = img.hasAttribute('alt');
+console.log(isAlt);
--- Origamid/javascript-completo/dom-iniciantes/004-foreach-e-arrow/assets/js/script.js
+++ Origamid/javascript-completo/dom-iniciantes/004-foreach-e-arrow/assets/js/script.js
@@ -1,20 +1,21 @@
-// Mostre no console cada parágrado do site
-const paragrafos = document.querySelectorAll('p');
-console.log(paragrafos);
+// Adicione a classe ativo a todos os itens do menu
+const menu = document.querySelectorAll('.menu a');
+menu.forEach((item) => item.classList.add('ativo'));
+console.log(menu);
 
-// Mostre o texto dos parágrafos no console
-paragrafos.forEach((item) => console.log(item.innerText))
+// Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
+menu.forEach((item, index) => {if(index !== 0) item.classList.remove('ativo')});
+console.log(menu);
 
-// Como corrigir os erros abaixo:
+// Verifique se as imagens possuem o atributo alt
 const imgs = document.querySelectorAll('img');
 
-imgs.forEach((item, index) => {
-  console.log(item, index);
+imgs.forEach((item) => {
+  const isAlt = item.hasAttribute('alt')
+  console.log(isAlt);
 });
 
-let i = 0;
-imgs.forEach(() => {
-  console.log(i++);
-});
-
-imgs.forEach(() => i++);
+// Modifique o href do link externo no menu
+const hrefExtern = document.querySelector('a[href^="http"]');
+hrefExtern.setAttribute('href', 'https://www.globo.com/')
+console.log(hrefExtern)
