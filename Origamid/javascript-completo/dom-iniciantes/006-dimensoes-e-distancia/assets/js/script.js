059b4f5e8e9895ac1568f25992ed4e91ce8936dd
Gabriel Chagas <gabrielserq@outlook.com>
2021-05-20 21:34:41 -0300

    chore: desafios da aula sobre dimensões e distâncias.

--- Origamid/javascript-completo/dom-iniciantes/006-dimensoes-e-distancia/assets/js/script.js
+++ Origamid/javascript-completo/dom-iniciantes/006-dimensoes-e-distancia/assets/js/script.js
@@ -1,20 +1,44 @@
-// Mostre no console cada parágrado do site
-const paragrafos = document.querySelectorAll('p');
-console.log(paragrafos);
+// Verifique a distância da primeira imagem
+// em relação ao topo da página
 
-// Mostre o texto dos parágrafos no console
-paragrafos.forEach((item) => console.log(item.innerText))
+const image = document.querySelector('img');
+console.log(image.offsetTop);
 
-// Como corrigir os erros abaixo:
-const imgs = document.querySelectorAll('img');
 
-imgs.forEach((item, index) => {
-  console.log(item, index);
+// Retorne a soma da largura de todas as imagens
+function somaImages() {
+  const images = document.querySelectorAll('img');
+  let soma = 0;
+  images.forEach((item) => {
+    soma += item.offsetWidth;
+  });
+  console.log(soma)
+}
+
+// evento que espera as imagens carregar para disparar a função de somaImages
+window.onload = function (){
+  somaImages();
+}
+
+
+// Verifique se os links da página possuem
+// o mínimo recomendado para telas utilizadas
+// com o dedo. (48px/48px de acordo com o google)
+
+const links = document.querySelectorAll('a');
+links.forEach((item) => {
+  if (item.offsetWidth >= 48 && item.offsetHeight >= 48) {
+    console.log(item, 'possui boa acessibilidade');
+  }else{
+    console.log(item, 'não possui boa acessibilidade')
+  }
 });
 
-let i = 0;
-imgs.forEach(() => {
-  console.log(i++);
-});
 
-imgs.forEach(() => i++);
+// Se o browser for menor que 720px,
+// adicione a classe menu-mobile ao menu
+
+if(window.matchMedia('(max-width: 720px)').matches){
+  let menu = document.querySelector('.menu');
+  menu.classList.add('menu-mobile');
+}
