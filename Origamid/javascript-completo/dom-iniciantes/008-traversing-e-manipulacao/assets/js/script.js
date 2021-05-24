d98ddf999dc82bec8dbbab2240706091454c18ff
Gabriel Chagas <gabrielserq@outlook.com>
2021-05-23 23:28:16 -0300

    chore: anotações e exemplos sobre traversing

---
+++ Origamid/javascript-completo/dom-iniciantes/008-traversing-e-manipulacao/assets/js/script.js
@@ -0,0 +1,15 @@
+const menu = document.querySelector('.menu');
+
+console.log(menu.innerHTML);
+console.log(menu.outerHTML);
+console.log(menu.innerText);
+
+const lista = document.querySelector('.animais-lista');
+
+console.log(lista.parentElement);
+console.log(lista.parentElement.parentElement);
+console.log(lista.parentElement.previousElementSibling);
+console.log(lista.parentElement.nextElementSibling);
+console.log(lista.children);
+console.log(lista.children[0]);
+console.log(lista.children[--lista.children.length]);
