d1d29fd17c01ec7b648a153f066f3a3c05ddba62
Gabriel Chagas <gabrielserq@outlook.com>
2021-05-25 12:01:01 -0300

    chore: exercícios sobre transversing e manipulação

--- Origamid/javascript-completo/dom-iniciantes/008-traversing-e-manipulacao/assets/js/script.js
+++ Origamid/javascript-completo/dom-iniciantes/008-traversing-e-manipulacao/assets/js/script.js
@@ -1,15 +1,20 @@
+// Duplique o menu e adicione ele em copy
 const menu = document.querySelector('.menu');
+const copy = document.querySelector('.copy');
 
-console.log(menu.innerHTML);
-console.log(menu.outerHTML);
-console.log(menu.innerText);
+const menuClone = menu.cloneNode(true);
+copy.appendChild(menuClone);
 
-const lista = document.querySelector('.animais-lista');
+// Selecione o primeiro DT da dl de Faq
+const faqList = document.querySelector('.faq-lista');
+const firstDT = faqList.querySelector('dt');
+console.log(firstDT)
 
-// console.log(lista.parentElement);
-// console.log(lista.parentElement.parentElement);
-// console.log(lista.parentElement.previousElementSibling);
-// console.log(lista.parentElement.nextElementSibling);
-// console.log(lista.children);
-// console.log(lista.children[0]);
-// console.log(lista.children[--lista.children.length]);
+// Selecione o DD referente ao primeiro DT
+const ddFirst = firstDT.nextElementSibling;
+console.log(ddFirst);
+
+// Substitua o conteúdo html de .faq pelo de .animais
+const faq = document.querySelector('.faq');
+const animais = document.querySelector('.animais');
+faq.innerHTML = animais.innerHTML;
