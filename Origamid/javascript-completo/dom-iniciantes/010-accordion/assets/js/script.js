254b9a46ebecfcf30eada0e7cbb6367ceb700a37
Gabriel Chagas <gabrielserq@outlook.com>
2021-06-03 15:19:13 -0300

    refactor: refatorando a função de initTabNav

--- Origamid/javascript-completo/dom-iniciantes/010-accordion/assets/js/script.js
+++ Origamid/javascript-completo/dom-iniciantes/010-accordion/assets/js/script.js
@@ -1,3 +1,5 @@
+//* Navegação por tab
+function initTabNav() {
 // recuperando todo o conteudo do tabMenu
 const tabMenu = document.querySelectorAll('.js-tabmenu li');
 
@@ -5,12 +7,14 @@ const tabMenu = document.querySelectorAll('.js-tabmenu li');
 const tabContent = document.querySelectorAll('.js-tabcontent section');
 
 // verificação para adicionar a classe ativo ao primeiro item
-if(tabMenu.length && tabContent.length)
+  if (tabMenu.length && tabContent.length)
   tabContent[0].classList.add('ativo')
 
 //função para adicionar a classe de ativo ao item 
-function activeTab(index){
-  tabContent.forEach((item) => {item.classList.remove('ativo')})
+  function activeTab(index) {
+    tabContent.forEach((item) => {
+      item.classList.remove('ativo')
+    })
   tabContent[index].classList.add('ativo');
 }
 
