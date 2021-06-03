090194fed05a394276b6a5ebba16a4977eefbe08
Gabriel Chagas <gabrielserq@outlook.com>
2021-06-03 15:19:37 -0300

    feat: iniciando as funções necessárias

--- Origamid/javascript-completo/dom-iniciantes/010-accordion/assets/js/script.js
+++ Origamid/javascript-completo/dom-iniciantes/010-accordion/assets/js/script.js
@@ -1,28 +1,29 @@
 //* Navegação por tab
 function initTabNav() {
-// recuperando todo o conteudo do tabMenu
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
+  // recuperando todo o conteudo do tabMenu
+  const tabMenu = document.querySelectorAll('.js-tabmenu li');
 
-// recuperando todo o conteudo do tabcontent
-const tabContent = document.querySelectorAll('.js-tabcontent section');
+  // recuperando todo o conteudo do tabcontent
+  const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-// verificação para adicionar a classe ativo ao primeiro item
+  // verificação para adicionar a classe ativo ao primeiro item
   if (tabMenu.length && tabContent.length)
-  tabContent[0].classList.add('ativo')
+    tabContent[0].classList.add('ativo')
 
-//função para adicionar a classe de ativo ao item 
+  //função para adicionar a classe de ativo ao item 
   function activeTab(index) {
     tabContent.forEach((item) => {
       item.classList.remove('ativo')
     })
-  tabContent[index].classList.add('ativo');
+    tabContent[index].classList.add('ativo');
+  }
+
+  // adicionando um evento para cada item 
+
+  tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener('click', () => activeTab(index));
+  });
 }
-
-// adicionando um evento para cada item 
-
-tabMenu.forEach((itemMenu, index) => {
-  itemMenu.addEventListener('click', () => activeTab(index));
-});
 
 //* Accordion
 function initAccordion() {
@@ -54,3 +55,6 @@ function initAccordion() {
 
 
 
+//* Iniciando as funções
+initTabNav();
+initAccordion();
