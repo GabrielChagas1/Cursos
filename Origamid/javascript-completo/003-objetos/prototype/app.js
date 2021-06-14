300411ddd3bcc25c117b8ff981c9cc94a3c0497a
Gabriel Chagas <gabrielserq@outlook.com>
2021-06-14 11:30:39 -0300

    feat: anotações da primeira aula sobre prototype

--- Origamid/javascript-completo/003-objetos/prototype/app.js
+++ Origamid/javascript-completo/003-objetos/prototype/app.js
@@ -1,1 +1,9 @@
-// anotações aula sobre prototype
+function Pessoa(nome, idade){
+  this.nome = nome;
+  this.idade = idade;
+}
+
+const gabriel = new Pessoa('Gabriel', 21);
+
+console.log(Pessoa.prototype)
+console.log(gabriel.prototype)
