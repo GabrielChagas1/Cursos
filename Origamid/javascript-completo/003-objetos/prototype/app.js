fb4b75729adaf39c00da260aeb7130f9a583b4da
Gabriel Chagas <gabrielserq@outlook.com>
2021-06-17 21:56:57 -0300

    feat: arquivo para os exercícios

--- Origamid/javascript-completo/003-objetos/prototype/app.js
+++ Origamid/javascript-completo/003-objetos/prototype/app.js
@@ -1,14 +1,22 @@
-function Pessoa(nome, idade){
-  this.nome = nome;
-  this.idade = idade;
-}
+// Crie uma função construtora de Pessoas
+// Deve conter nome, sobrenome e idade
+// Crie um método no protótipo que retorne
+// o nome completo da pessoa
 
-const gabriel = new Pessoa('Gabriel', 21);
+// Liste os métodos acessados por 
+// dados criados com NodeList,
+// HTMLCollection, Document
 
-const pais = 'Brasil';
-const cidade = new String('Rio');
+// Liste os construtores dos dados abaixo
+const li = document.querySelector('li');
 
-console.log(pais.charAt(0));
-console.log(cidade.charAt(0));
+li;
+li.click;
+li.innerText;
+li.value;
+li.hidden;
+li.offsetLeft;
+li.click();
 
-console.log(String.prototype);
+// Qual o construtor do dado abaixo:
+li.hidden.constructor.name;
