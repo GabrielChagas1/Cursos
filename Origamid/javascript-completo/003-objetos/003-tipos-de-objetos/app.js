4b438e9ae3ffcd12635488a2e75f7308aae4727c
Gabriel Chagas <gabrielserq@outlook.com>
2021-06-19 17:40:27 -0300

    feat: subindo arquivos para a aula sobre tipos de objetos

---
+++ Origamid/javascript-completo/003-objetos/003-tipos-de-objetos/app.js
@@ -0,0 +1,44 @@
+// Crie uma função construtora de Pessoas
+// Deve conter nome, sobrenome e idade
+// Crie um método no protótipo que retorne
+// o nome completo da pessoa
+
+function Pessoa(nome, sobrenome, idade){
+  this.nome = nome;
+  this.sobrenome = sobrenome;
+  this.idade = idade;
+}
+
+Pessoa.prototype.completedName = function(){
+  return `${this.nome} ${this.sobrenome}`;
+}
+
+const pessoa = new Pessoa('Gabriel', 'Chagas', 21);
+
+// Liste os métodos acessados por 
+// dados criados com NodeList,
+// HTMLCollection, Document
+
+// console.log(NodeList.prototype);
+// console.log(HTMLCollection.prototype);
+// console.log(Document.prototype);
+
+// Liste os construtores dos dados abaixo
+const li = document.querySelector('li');
+
+li;//htmlliElement
+li.click;//function
+li.innerText;//string
+li.value;//number
+li.hidden;//boolean
+li.offsetLeft;//number
+li.click();//undefined
+
+
+
+
+
+
+
+// Qual o construtor do dado abaixo:
+li.hidden.constructor.name;//string
