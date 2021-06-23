3b2ad469698117b11afc68c50e361b7f2aebee1a
Gabriel Chagas <gabrielserq@gmail.com>
2021-06-23 01:07:43 -0300

    feat: adicionando um try catch para recuperar a mensagem do erro

--- Oministack/NLW/NLW6/nodejs/nlw_valoriza/src/controllers/CreateUserController.ts
+++ Oministack/NLW/NLW6/nodejs/nlw_valoriza/src/controllers/CreateUserController.ts
@@ -4,17 +4,21 @@ import { CreateUserService } from "../services/CreateUserService";
 
 class CreateUserController{
   async handle(request: Request, response: Response){
-    // recuperando os valores que chegam do body da aplicação
-    const { name, email, admin} = request.body; 
+    try {
+      // recuperando os valores que chegam do body da aplicação
+      const { name, email, admin} = request.body; 
 
-    // instanciando um objeto do user service
-    const createUserService = new CreateUserService();
+      // instanciando um objeto do user service
+      const createUserService = new CreateUserService();
 
-    // utilizando o método execute para criar um novo usuário
-    const user = await createUserService.execute({name, email, admin});
+      // utilizando o método execute para criar um novo usuário
+      const user = await createUserService.execute({name, email, admin});
 
-    // retornando os dados cadastrados
-    return response.json(user);
+      // retornando os dados cadastrados
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({error: err.message});
+    }
   }
 }
 
