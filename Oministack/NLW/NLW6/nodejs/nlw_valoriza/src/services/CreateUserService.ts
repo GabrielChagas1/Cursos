ede680d16dbb5e3f0a90baa10e8aa65da0a1b0bb
Gabriel Chagas <gabrielserq@outlook.com>
2021-06-28 15:19:26 -0300

    feat: recebendo o password no corpo da requisição

--- Oministack/NLW/NLW6/nodejs/nlw_valoriza/src/services/CreateUserService.ts
+++ Oministack/NLW/NLW6/nodejs/nlw_valoriza/src/services/CreateUserService.ts
@@ -4,11 +4,12 @@ import { UsersRepositories } from "../repositories/UsersRepositories"
 interface IUserRequest{
   name: string;
   email: string;
+  password: string;
   admin?: boolean
 }
 
 class CreateUserService{
-  async execute({name, email, admin} : IUserRequest){
+  async execute({name, email, password, admin} : IUserRequest){
 
     // criando uma instância do repositório de users
     const usersRepository = getCustomRepository(UsersRepositories);
@@ -29,6 +30,7 @@ class CreateUserService{
     const user = usersRepository.create({
       name,
       email,
+      password,
       admin
     });
 
