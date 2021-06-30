c0b8047c25de66e08ed7dafcb046d879a7b0fad3
Gabriel Chagas <gabrielserq@outlook.com>
2021-06-29 23:11:34 -0300

    feat: criando rota de post para cadastrar um compliment

--- Oministack/NLW/NLW6/nodejs/nlw_valoriza/src/routes.ts
+++ Oministack/NLW/NLW6/nodejs/nlw_valoriza/src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticaUserController";
+import { CreateComplimentController } from "./controllers/CreateComplimentController";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
@@ -8,12 +9,14 @@ const router = Router();
 
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
+const createComplimentController = new CreateComplimentController();
 const authenticateUserController = new AuthenticateUserController();
 
 
 router.post("/tags", ensureAdmin, createTagController.handle);
 
 router.post("/users", createUserController.handle);
+router.post("/compliments", createComplimentController.handle);
 router.post("/login", authenticateUserController.handle);
 
 export {router};
