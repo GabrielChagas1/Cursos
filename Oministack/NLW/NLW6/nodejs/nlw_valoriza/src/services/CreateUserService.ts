5c163abe4b0aa6a0a2e9cda8d8ca5d9c33d91b82
Gabriel Chagas <gabrielserq@outlook.com>
2021-06-29 23:10:11 -0300

    refactor: ajustando para o admin ser falso, se na request não tiver o parametro de admin

--- Oministack/NLW/NLW6/nodejs/nlw_valoriza/src/services/CreateUserService.ts
+++ Oministack/NLW/NLW6/nodejs/nlw_valoriza/src/services/CreateUserService.ts
@@ -10,7 +10,7 @@ interface IUserRequest{
 }
 
 class CreateUserService{
-  async execute({name, email, password, admin} : IUserRequest){
+  async execute({name, email, password, admin = false} : IUserRequest){
 
     // criando uma instância do repositório de users
     const usersRepository = getCustomRepository(UsersRepositories);
