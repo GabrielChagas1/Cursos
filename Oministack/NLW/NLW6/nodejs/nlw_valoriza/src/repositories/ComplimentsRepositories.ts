a1df3809bee8a8d0c533ffed865e8ddee0a4ed79
Gabriel Chagas <gabrielserq@outlook.com>
2021-07-11 22:31:21 -0300

    chore: cometando os arquivos

--- Oministack/NLW/NLW6/nodejs/nlw_valoriza/src/repositories/ComplimentsRepositories.ts
+++ Oministack/NLW/NLW6/nodejs/nlw_valoriza/src/repositories/ComplimentsRepositories.ts
@@ -1,7 +1,7 @@
 import { EntityRepository, Repository } from "typeorm";
 import { Compliment } from "../entities/Compliement";
 
-
+// criando classe para o repositório de compliments
 @EntityRepository(Compliment)
 class ComplimentsRepositories extends Repository<Compliment>{
 
