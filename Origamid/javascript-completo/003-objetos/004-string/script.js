daba77915f4775ab6ee18d328b750d3e08ccc0ec
Gabriel Chagas <gabrielserq@outlook.com>
2022-02-13 15:20:56 -0300

    exercises about string

--- Origamid/javascript-completo/003-objetos/004-string/script.js
+++ Origamid/javascript-completo/003-objetos/004-string/script.js
@@ -24,25 +24,21 @@ const transacoes = [
   },
 ];
 
-let taxaTotal = 0;
-let recebimentoTotal = 0;
-transacoes.forEach((item) => {
-  const numeroLimpo = +item.valor.replace('R$ ', '');
-  if (item.descricao.slice(0, 4) === 'Taxa') {
-    taxaTotal += numeroLimpo;
-  } else if(item.descricao.slice(0, 4) === 'Rece') {
-    recebimentoTotal += numeroLimpo;
-  }
-})
-console.log(taxaTotal)
-console.log(recebimentoTotal)
+let taxa = 0;
+let recebimento = 0;
+
+// transacoes.forEach((item) => item.descricao.split(' ')[0] == 'Taxa' ? taxa += Number(item.valor.split(' ')[1]): recebimento += Number(item.valor.split(' ')[1]));
+transacoes.forEach((item) => item.descricao.slice(0, 4) === 'Taxa' ? taxa += +item.valor.replace('R$ ', '') : recebimento += +item.valor.replace('R$ ', ''));
+console.log(taxa);
+console.log(recebimento)
+
+
 
 
 // Retorne uma array com a lista abaixo
 const transportes = 'Carro;Avião;Trem;Ônibus;Bicicleta';
-
-const arrayTransportes = transportes.split(';');
-console.log(arrayTransportes);
+const array = transportes.split(';');
+console.log(array)
 
 // Substitua todos os span's por a's
 let html = `<ul>
@@ -50,25 +46,20 @@ let html = `<ul>
                 <li><span>Produtos</span></li>
                 <li><span>Contato</span></li>
               </ul>`;
-
 html = html.split('span').join('a');
+console.log(html)
 
-console.log(html);
 
 // Retorne o último caracter da frase
 const frase = 'Melhor do ano!';
-
 console.log(frase.slice(-1));
+
 
 // Retorne o total de taxas
 const transacoes2 = ['Taxa do Banco', '   TAXA DO PÃO', '  taxa do mercado', 'depósito Bancário', 'TARIFA especial'];
 
-let taxasTotal = 0;
-transacoes2.forEach((item) => {
-  item = item.toLowerCase().trim().slice(0,4);
+let totalTaxas = 0;
+transacoes2.forEach((item) => item.trim().toLowerCase().includes('taxa') ? totalTaxas++ : null);
+console.log(totalTaxas)
 
-  if(item === 'taxa')
-    taxasTotal++
-})
 
-console.log(taxasTotal)
