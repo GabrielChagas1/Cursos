cc3ef3a9924b37060d42e1927a0f4dcfb237050d
Gabriel Chagas <gabrielserq@outlook.com>
2022-06-23 17:26:36 -0300

    creating code during classroom

---
+++ Origamid/javascript-completo/003-objetos/006-array/script-aula.js
@@ -0,0 +1,23 @@
+// const instrumentos = ['Guitarra', 'Baixo', 'Violão'];
+// const precos = [49, 99, 69, 89];
+
+// const dados = [new String('Tipo 1'), ['Carro', 'Portas', {cor: 'Azul', preco: 2000}], function andar(nome) { console.log(nome) }];
+
+// dados[2]('Ford');
+// dados[1][2].cor; // azul
+
+// const carros = new Array('Ford', 'Fiat', 'Honda');
+
+// carros[2] = 'Passati';
+// carros[20] = 'Kia'
+
+
+// const frutas = ['Banana', 'Pêra', ['Uva Roxa', 'Uva Verde']]
+
+const carros = ['Ford', 'Fiat', 'VW'];
+carros.unshift('Honda', 'Kia'); // 5
+
+const linguagens = ['html', 'css', 'js', 'php', 'python',
+'js'];
+
+
